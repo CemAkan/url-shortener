f29354b1c6b1869fd9e15286cc198d2c0829171c
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-09 16:18:55 +0300

    feat(config): add logs folder checker and creator if it not exist

--- config/logger.go
+++ config/logger.go
@@ -9,13 +9,19 @@ import (
 var Log = logrus.New()
 
 func InitLogger() {
+	//first output target
 	Log.SetOutput(os.Stdout)
-
 	Log.SetLevel(logrus.InfoLevel)
 
+	//formatter
 	Log.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: time.RFC3339,
 		ForceColors:     true,
 	})
+
+	//logs folder checker & creator
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		_ = os.Mkdir("logs", 0775)
+	}
 }
