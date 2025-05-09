54cb1c9ecfd9b0b5b577fd628ce5fb1eebee98bf
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-09 17:22:19 +0300

    feat(db): initialize PostgreSQL connection via GORM

--- config/db.go
+++ config/db.go
@@ -1,5 +1,25 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 var DB *gorm.DB
+
+func InitDB() {
+
+	// complete dsn url with getting values from env
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		GetEnv("DB_HOST", ""), GetEnv("DB_USER", ""), GetEnv("DB_PASSWORD", ""), GetEnv("DB_NAME", ""), GetEnv("DB_PORT", ""), GetEnv("DB_SSLMODE", ""))
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		Log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	DB = db
+	Log.Info("Database connection established successfully")
+}
