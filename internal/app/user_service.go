1dc908d2f0ff8d5aaf324f420255dca9da991f2e
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-10 02:41:00 +0300

    feat(service): implement userService struct and constructor with UserRepository dependency

--- internal/app/user_service.go
+++ internal/app/user_service.go
@@ -1,8 +1,21 @@
 package app
 
-import "github.com/CemAkan/url-shortener/internal/domain"
+import (
+	"github.com/CemAkan/url-shortener/internal/domain"
+	"github.com/CemAkan/url-shortener/internal/repository"
+)
 
 type UserService interface {
 	Register(username, password string) (*domain.User, error)
 	Login(username, password string) (*domain.User, error)
+}
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{
+		repo: userRepo,
+	}
 }
