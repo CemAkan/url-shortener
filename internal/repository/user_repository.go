b67972e4fd580079cdd71342e413d40bbd761fea
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-09 20:18:44 +0300

    feat(repository): implement userRepo struct and constructor with global DB injection

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,10 +1,23 @@
 package repository
 
-import "github.com/CemAkan/url-shortener/internal/domain"
+import (
+	"github.com/CemAkan/url-shortener/config"
+	"github.com/CemAkan/url-shortener/internal/domain"
+	"gorm.io/gorm"
+)
 
 type UserRepository interface {
 	Create(user *domain.User) error
 	FindByID(id uint) (*domain.User, error)
 	FindByUsername(username string) (*domain.User, error)
 	ListAllUsers() ([]domain.User, error)
+}
+
+type userRepo struct {
+	db *gorm.DB
+}
+
+func NewUserRepository() UserRepository {
+	return &userRepo{
+		db: config.DB
 }
