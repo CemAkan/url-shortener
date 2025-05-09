63e203d432c75df5c3cb3a8d8be4d9808465eaac
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-09 16:09:21 +0300

    feat(config): implement InitLogger with text formatter and stdout output

--- config/logger.go
+++ config/logger.go
@@ -1,5 +1,19 @@
 package config
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
 
 var Log = logrus.New()
+
+func InitLogger() {
+	Log.SetOutput(os.Stdout)
+
+	Log.SetLevel(logrus.InfoLevel)
+
+	Log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+}
