6260030e8555647f4d3d51e45b7336d8f1bd38a9
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-09 20:45:27 +0300

    fix(repository): add missing comma in NeewUserRepository func's return userRepo struct parameter

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -20,41 +20,41 @@ type userRepo struct {
 
 func NewUserRepository() UserRepository {
 	return &userRepo{
-		db: config.DB
-}
+		db: config.DB,
+	}
 }
 
 // Create inserts new user
-func (r *userRepo) Create (user *domain.User) error{
+func (r *userRepo) Create(user *domain.User) error {
 	return r.db.Create(user).Error
 }
 
 // FindByID retrieves user by ID
-func (r *userRepo)FindByID(id uint) (*domain.User, error){
+func (r *userRepo) FindByID(id uint) (*domain.User, error) {
 	var user domain.User
-	err := r.db.First(&user,id).Error
-		return &user, err
+	err := r.db.First(&user, id).Error
+	return &user, err
 }
 
 // FindByUsername retrieves user by username
-func (r *userRepo) FindByUsername (username string)(*domain.User, error){
+func (r *userRepo) FindByUsername(username string) (*domain.User, error) {
 	var user domain.User
 
-	err := r.db.Where("username=%s",username).First(&user).Error
+	err := r.db.Where("username=%s", username).First(&user).Error
 
-	return &user,err
+	return &user, err
 }
 
 // Update modifies to existing user
-func (r *userRepo) Update (user *domain.User)error{
+func (r *userRepo) Update(user *domain.User) error {
 	return r.db.Save(user).Error
 }
 
 // ListAllUsers retrieves all user records
-func (r *userRepo) ListAllUsers() ([]domain.User, error){
+func (r *userRepo) ListAllUsers() ([]domain.User, error) {
 	var users []domain.User
 
 	err := r.db.Find(&users).Error
 
-	return users,err
+	return users, err
 }
