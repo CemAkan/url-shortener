0f97defc744d1c96fef649ea29cd007af8648e7e
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-10 04:13:00 +0300

    feat(handler): implement Register HTTP handler to create new user

--- internal/delivery/auth_handler.go
+++ internal/delivery/auth_handler.go
@@ -1,6 +1,9 @@
 package delivery
 
-import "github.com/CemAkan/url-shortener/internal/app"
+import (
+	"github.com/CemAkan/url-shortener/internal/app"
+	"github.com/gofiber/fiber/v2"
+)
 
 type AuthHandler struct {
 	service app.UserService
@@ -11,4 +14,27 @@ func NewAuthHandler(userService app.UserService) *AuthHandler {
 	return &AuthHandler{
 		service: userService,
 	}
+}
+
+// Register handles user create request
+func (h *AuthHandler) Register(c *fiber.Ctx) error {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid request body"})
+	}
+
+	user, err := h.service.Register(req.Username, req.Password)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"id":       user.ID,
+		"username": user.Username,
+	})
 }
