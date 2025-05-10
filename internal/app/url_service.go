638f3676b105b3032ce9316758bf12866c9bdb9f
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-11 00:29:57 +0300

    feat(service): implement urlService struct and constructor with dependency injection

--- internal/app/url_service.go
+++ internal/app/url_service.go
@@ -1,9 +1,22 @@
 package app
 
-import "github.com/CemAkan/url-shortener/internal/domain"
+import (
+	"github.com/CemAkan/url-shortener/internal/domain"
+	"github.com/CemAkan/url-shortener/internal/repository"
+)
 
 type URLService interface {
 	Shorten(originalURL string, userID uint) (*domain.URL, error)
 	GetByCode(code string) (*domain.URL, error)
 	GetByUserID(userID uint) ([]domain.URL, error)
+}
+
+type urlService struct {
+	repo repository.URLRepository
+}
+
+func NewURLService(urlRepo repository.URLRepository) URLService {
+	return &userService{
+		repo: urlRepo,
+	}
 }
