e845e7d040b781441caa0b143fcd642d7ca345ba
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-10 06:55:30 +0300

    feat(middleware): check and validate Authorization header format

--- internal/middleware/jwt_auth.go
+++ internal/middleware/jwt_auth.go
@@ -1,9 +1,18 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"strings"
+)
 
 func JWTAuth() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+
+		authHeader := c.Get("Authorization")
+
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing or invalid Authorization header"})
+		}
 
 		return c.Next()
 	}
