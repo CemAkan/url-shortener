f2bfb7d101327847724b791c9698a0d55ca9c63c
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-10 04:20:25 +0300

    feat(handler): implement Login HTTP handler for user authentication

--- internal/delivery/auth_handler.go
+++ internal/delivery/auth_handler.go
@@ -38,3 +38,26 @@ func (h *AuthHandler) Register(c *fiber.Ctx) error {
 		"username": user.Username,
 	})
 }
+
+// Login handles to request for user logins
+func (h *AuthHandler) Login(c *fiber.Ctx) error {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid request body"})
+	}
+
+	user, err := h.service.Login(req.Username, req.Password)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"id":       user.ID,
+		"username": user.Username,
+	})
+}
