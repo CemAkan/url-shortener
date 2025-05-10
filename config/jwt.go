e06efcf2e1620760dd90241e3f30a6a77e8e3383
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-10 05:30:25 +0300

    feat(jwt): implement GenerateJWT to create signed token with user ID and 24h expiration

---
+++ config/jwt.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var jwtSecret = []byte(GetEnv("JWT_SECRET", "default_jwt_secret123!@#"))
+
+func GenerateJWT(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(jwtSecret)
+
+}
