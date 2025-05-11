992c39465fbc60d021e480efb679971ec00bbcaa
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-11 15:06:51 +0300

    feat(handler): add GET /my/urls endpoint to list user's shortened URLs

--- internal/delivery/url_handler.go
+++ internal/delivery/url_handler.go
@@ -15,6 +15,7 @@ func NewURLService(urlService app.URLService) *URLhandler {
 	}
 }
 
+// Shorten handles request to create short url
 func (h *URLhandler) Shorten(c *fiber.Ctx) error {
 	var req struct {
 		OriginalURL string  `json:"original_url"`
@@ -38,4 +39,17 @@ func (h *URLhandler) Shorten(c *fiber.Ctx) error {
 		"original_url": req.OriginalURL,
 		"short_url":    c.BaseURL() + "/" + url.Code,
 	})
+}
+
+// ListUserURLs handles request to list user's all urls
+func (h *URLhandler) ListUserURLs(c *fiber.Ctx) error {
+	userID := c.Locals("user_id").(uint)
+
+	urls, err := h.service.GetUserURLs(userID)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(urls)
 }
