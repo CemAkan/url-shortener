cf7d1a027cf352013db456ce30b0c8f19e0b6351
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-11 14:51:02 +0300

    feat(handler): implement POST /shorten endpoint to create short URLs

--- internal/delivery/url_handler.go
+++ internal/delivery/url_handler.go
@@ -1,6 +1,9 @@
 package delivery
 
-import "github.com/CemAkan/url-shortener/internal/app"
+import (
+	"github.com/CemAkan/url-shortener/internal/app"
+	"github.com/gofiber/fiber/v2"
+)
 
 type URLhandler struct {
 	service app.URLService
@@ -10,4 +13,29 @@ func NewURLService(urlService app.URLService) *URLhandler {
 	return &URLhandler{
 		service: urlService,
 	}
+}
+
+func (h *URLhandler) Shorten(c *fiber.Ctx) error {
+	var req struct {
+		OriginalURL string  `json:"original_url"`
+		CustomCode  *string `json:"custom_code,omitempty"`
+	}
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid request"})
+	}
+
+	userID := c.Locals("user_id").(uint)
+
+	url, err := h.service.Shorten(req.OriginalURL, userID, req.CustomCode)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"code":         url.Code,
+		"original_url": req.OriginalURL,
+		"short_url":    c.BaseURL() + "/" + url.Code,
+	})
 }
