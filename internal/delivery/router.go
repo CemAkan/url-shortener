74aca81efd35e169b2ee5dc1411bd6edee55a049
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-11 15:10:31 +0300

    feat(router): register URL endpoints with JWT middleware

--- internal/delivery/router.go
+++ internal/delivery/router.go
@@ -5,13 +5,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupRoutes(app *fiber.App, authHandler *AuthHandler) {
+func SetupRoutes(app *fiber.App, authHandler *AuthHandler, urlHandler *URLhandler) {
 	api := app.Group("/api")
 
 	// public routes (no need jwt)
+
+	//user
 	api.Post("/register", authHandler.Register)
 	api.Post("/login", authHandler.Login)
 
 	// protected routes (jwt required)
 	api.Get("/me", middleware.JWTAuth(), authHandler.Me)
+
+	//url
+	api.Post("/shorten", middleware.JWTAuth(), urlHandler.Shorten)
+	api.Get("/my/urls", middleware.JWTAuth(), urlHandler.ListUserURLs)
 }
