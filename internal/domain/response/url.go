6ed9cb5f7640992003be4ea1a8eb2feb8db05677
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 00:55:14 +0300

    refactor(domain): make URLResponse more suitable for Shorten handler

--- internal/domain/response/url.go
+++ internal/domain/response/url.go
@@ -3,7 +3,7 @@ package response
 type URLResponse struct {
 	Code        string `json:"code" example:"abc123"`
 	OriginalURL string `json:"original_url" example:"https://google.com"`
-	DailyClicks int    `json:"daily_clicks" example:"10"`
+	ShortURL    string `json:"short_url" example:"https://localhost/abc123"`
 }
 
 type DetailedURLResponse struct {
