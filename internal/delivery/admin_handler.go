74c624303ba418c29e5f144f25dee2a072e6f479
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-12 07:57:14 +0300

    feat(handle): return parameters make more comprehensive for collect all data records

--- internal/delivery/admin_handler.go
+++ internal/delivery/admin_handler.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"github.com/CemAkan/url-shortener/internal/app"
+	"github.com/CemAkan/url-shortener/internal/domain"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -18,12 +19,29 @@ func NewAdminHandler(userService app.UserService, urlService app.URLService) *Ad
 	}
 }
 
-// ListUsers lists all users
+// ListUsers lists all users nad their links
 func (h *AdminHandler) ListUsers(c *fiber.Ctx) error {
 	users, err := h.userService.ListAllUsers()
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	return c.JSON(users)
+	type res struct {
+		User domain.User
+		Urls []domain.URL
+	}
+
+	var resp res
+	var resps []res
+
+	for _, user := range users {
+		urls, _ := h.urlService.GetUserURLs(user.ID)
+
+		resp.User = user
+		resp.Urls = urls
+
+		resps = append(resps, resp)
+	}
+
+	return c.JSON(resps)
 }
