3f3589d8bbcbb81a4e0da46f3ed68f780946df6f
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-12 06:50:31 +0300

    feat(utils): add DeleteURLCache helper for cleaning Redis keys related to URL

--- internal/utils/redis_cleaner.go
+++ internal/utils/redis_cleaner.go
@@ -1,5 +1,20 @@
-package main
+package utils
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/CemAkan/url-shortener/pkg/infrastructure"
+)
 
+func DeleteURLCache(code string) {
+	keys := []string{
+		fmt.Sprintf("clicks:%s", code),
+		fmt.Sprintf("hotlink:%s", code),
+	}
+
+	for _, key := range keys {
+		if err := infrastructure.Redis.Del(context.Background(), key).Err(); err != nil {
+			infrastructure.Log.Warnf("Failed to delete Redis key %s: %v", key, err)
+		}
+	}
 }
