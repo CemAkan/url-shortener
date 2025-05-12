b6573940afa44f5e54de74856fb43d6eb3e4821d
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 01:49:12 +0300

    docs(auth): add Swagger documentation for Register, Login and Me endpoints

--- internal/delivery/auth_handler.go
+++ internal/delivery/auth_handler.go
@@ -19,6 +19,16 @@ func NewAuthHandler(userService app.UserService) *AuthHandler {
 	}
 }
 
+// Register godoc
+// @Summary User Registration
+// @Description Creates a new user account
+// @Tags Auth
+// @Accept json
+// @Produce json
+// @Param request body request.AuthRequest true "User Credentials"
+// @Success 201 {object} response.UserResponse
+// @Failure 400 {object} response.ErrorResponse
+// @Router /register [post]
 func (h *AuthHandler) Register(c *fiber.Ctx) error {
 	var req request.AuthRequest
 
@@ -38,6 +48,16 @@ func (h *AuthHandler) Register(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(res)
 }
 
+// Login godoc
+// @Summary User Login
+// @Description Authenticates a user and returns JWT token
+// @Tags Auth
+// @Accept json
+// @Produce json
+// @Param request body request.AuthRequest true "User Credentials"
+// @Success 201 {object} response.LoginResponse
+// @Failure 400 {object} response.ErrorResponse
+// @Router /login [post]
 func (h *AuthHandler) Login(c *fiber.Ctx) error {
 	var req request.AuthRequest
 
@@ -63,6 +83,15 @@ func (h *AuthHandler) Login(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(res)
 }
 
+// Me godoc
+// @Summary Get current user's profile
+// @Description Returns authenticated user's profile info
+// @Tags Auth
+// @Produce json
+// @Success 200 {object} response.UserResponse
+// @Failure 500 {object} response.ErrorResponse
+// @Security BearerAuth
+// @Router /me [get]
 func (h *AuthHandler) Me(c *fiber.Ctx) error {
 	// getting userId which comes from middleware
 	id := c.Locals("user_id").(uint)
