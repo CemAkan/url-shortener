07e253951f3a2abae9849cf46f6cde9e44ff6d34
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-12 09:26:26 +0300

    feat(utils): implement GetAllClickKeys and DeleteClickKey functions for Redis cache operations

--- internal/utils/redis_click.go
+++ internal/utils/redis_click.go
@@ -23,3 +23,14 @@ func GetDailyClickCount(ctx context.Context, code string) (int, error) {
 	key := fmt.Sprintf("clicks:%s", code)
 	return infrastructure.Redis.Get(ctx, key).Int()
 }
+
+// GetAllClickKeys gets all urls click counter records from redis
+func GetAllClickKeys(ctx context.Context) ([]string, error) {
+	return infrastructure.Redis.Keys(ctx, "clicks:*").Result()
+}
+
+// DeleteClickKey deletes click record
+func DeleteClickKey(ctx context.Context, code string) error {
+	key := fmt.Sprintf("clicks:%s", code)
+	return infrastructure.Redis.Del(ctx, key).Err()
+}
