61853562a18ae8d6e6d748b0d888543a579289d7
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-12 07:22:04 +0300

    feat(handler): add AdminHandler struct and its constructor

--- internal/delivery/admin_handler.go
+++ internal/delivery/admin_handler.go
@@ -1,5 +1,16 @@
-package main
+package delivery
 
-func main() {
+import "github.com/CemAkan/url-shortener/internal/app"
 
+type AdminHandler struct {
+	userService app.UserService
+	urlService  app.URLService
+}
+
+// NewAdminHandler constructor
+func NewAdminHandler(userService app.UserService, urlService app.URLService) *AdminHandler {
+	return &AdminHandler{
+		userService: userService,
+		urlService:  urlService,
+	}
 }
