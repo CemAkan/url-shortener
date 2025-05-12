7fca4dfe62d5a6f1e09d06765e25bf17edde4b91
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-12 06:27:48 +0300

    fix(health): fix infrastructure import name and remove success log

--- internal/health/watchdog.go
+++ internal/health/watchdog.go
@@ -3,12 +3,11 @@ package health
 import (
 	"context"
 	"github.com/CemAkan/url-shortener/pkg/infrastructure"
-	infrastructure2 "github.com/CemAkan/url-shortener/pkg/infrastructure"
 	"time"
 )
 
 const (
-	HealthCheckInterval = 10 * time.Second
+	HealthCheckInterval = 60 * time.Second
 	MaxFailures         = 3
 )
 
@@ -19,26 +18,26 @@ func StartWatchdog(ctx context.Context, cancel context.CancelFunc) {
 
 	failures := 0
 
-	infrastructure2.Log.Info("Health Watchdog started")
+	infrastructure.Log.Info("Health Watchdog started")
 
 	for {
 		select {
 		case <-ctx.Done():
-			infrastructure2.Log.Info("Watchdog context cancelled, stopping health checks")
+			infrastructure.Log.Info("Watchdog context cancelled, stopping health checks")
 			return
 
 		case <-ticker.C:
 			if !checkHealth(ctx) {
 				failures++
-				infrastructure2.Log.Warnf("Healthcheck failed (%d/%d)", failures, MaxFailures)
+				infrastructure.Log.Warnf("Healthcheck failed (%d/%d)", failures, MaxFailures)
 				if failures >= MaxFailures {
-					infrastructure2.Log.Error("Failure threshold reached. Triggering shutdown via context cancel")
+					infrastructure.Log.Error("Failure threshold reached. Triggering shutdown via context cancel")
 					cancel()
 					return
 				}
 			} else {
 				failures = 0
-				infrastructure2.Log.Info("Healthcheck passed: DB & Redis OK")
+				// infrastructure.Log.Info("Healthcheck passed: DB & Redis OK")
 			}
 		}
 	}
@@ -50,15 +49,15 @@ func checkHealth(ctx context.Context) bool {
 	// db health check
 	sqlDB, err := infrastructure.DB.DB()
 	if err != nil || sqlDB.PingContext(ctx) != nil {
-		infrastructure2.Log.WithError(err).Error("Database healthcheck failed")
+		infrastructure.Log.WithError(err).Error("Database healthcheck failed")
 		healthy = false
 	}
 
 	//redis health check
 	rCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	defer cancel()
-	if err := infrastructure2.Redis.Ping(rCtx).Err(); err != nil {
-		infrastructure2.Log.WithError(err).Error("Redis healthcheck failed")
+	if err := infrastructure.Redis.Ping(rCtx).Err(); err != nil {
+		infrastructure.Log.WithError(err).Error("Redis healthcheck failed")
 		healthy = false
 	}
 
