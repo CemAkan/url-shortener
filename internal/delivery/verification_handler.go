9c9c49267f244e199ae66f454bbafbfadeb97332
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 23:11:29 +0300

    feat(handler): add VerifyMailAddress method to handle mail verification requests

--- internal/delivery/verification_handler.go
+++ internal/delivery/verification_handler.go
@@ -1,6 +1,10 @@
 package delivery
 
-import "github.com/CemAkan/url-shortener/internal/app"
+import (
+	"github.com/CemAkan/url-shortener/internal/app"
+	"github.com/CemAkan/url-shortener/internal/domain/response"
+	"github.com/gofiber/fiber/v2"
+)
 
 type VerificationHandler struct {
 	userService app.UserService
@@ -13,4 +17,19 @@ func NewVerificationHandler(userService app.UserService, mailService app.MailSer
 		userService: userService,
 		mailService: mailService,
 	}
+}
+
+func (h *VerificationHandler) VerifyMailAddress(c *fiber.Ctx) error {
+	userID := c.Locals("user_id").(uint)
+	_, err := h.userService.GetByID(userID)
+
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(response.ErrorResponse{Error: "User not found"})
+	}
+
+	if err := h.userService.SetTrueEmailConfirmation(userID); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(response.ErrorResponse{Error: "Database error"})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(response.SuccessResponse{Message: "mail confirmation successfully"})
 }
