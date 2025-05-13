e3d1dea84b8c56a7cdc9176c9781e8a966f8d107
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 07:06:25 +0300

    refactor(jwt): add expire time duration field to GenerateJWT function to generate jwt token for special uses

--- config/jwt.go
+++ config/jwt.go
@@ -7,11 +7,11 @@ import (
 
 var jwtSecret = []byte(GetEnv("JWT_SECRET", "default_jwt_secret123!@#"))
 
-// GenerateJWT creates signed token with user ID and 24h expiration
-func GenerateJWT(userID uint) (string, error) {
+// GenerateJWT creates signed token with user ID and special expire time duration
+func GenerateJWT(userID uint, duration time.Duration) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
-		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+		"exp":     time.Now().Add(duration).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -20,7 +20,7 @@ func GenerateJWT(userID uint) (string, error) {
 
 }
 
-// GenerateJWT parses and verifies signed tokens
+// ValidateJWT parses and verifies signed tokens
 func ValidateJWT(token string) (*jwt.Token, error) {
 	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		// signature method checking
