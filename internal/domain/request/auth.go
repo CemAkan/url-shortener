6f8e1ba31333477f604ca6be1505f8c2c5101b2e
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 08:34:29 +0300

    refactor(request): change fields via new user model

--- internal/domain/request/auth.go
+++ internal/domain/request/auth.go
@@ -1,6 +1,9 @@
 package request
 
+// AuthRequest  ➜  Kayıt / giriş istek gövdesi
 type AuthRequest struct {
-	Username string `json:"username" example:"asko"`
-	Password string `json:"password" example:"supersecret"`
+	Name     string `json:"name"     example:"Cem"`
+	Surname  string `json:"surname"  example:"Akan"`
+	Email    string `json:"email"    validate:"required,email" example:"asko@kusko.com"`
+	Password string `json:"password" validate:"required,min=8" example:"supersecret"`
 }
