d9a4abaefb1718d484300571dc01661437d928aa
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 21:22:27 +0300

    refactor(jwt): add purpose field in GenerateToken and ValidateToken functions

--- config/jwt.go
+++ config/jwt.go
@@ -1,32 +1,40 @@
 package config
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
+	"os"
 	"time"
 )
 
-var jwtSecret = []byte(GetEnv("JWT_SECRET", "default_jwt_secret123!@#"))
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
-// GenerateJWT creates signed token with user ID and special expire time duration
-func GenerateJWT(userID uint, duration time.Duration) (string, error) {
+func GenerateToken(userID uint, expiresIn time.Duration, purpose string) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
-		"exp":     time.Now().Add(duration).Unix(),
+		"exp":     time.Now().Add(expiresIn).Unix(),
+		"type":    purpose,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
 	return token.SignedString(jwtSecret)
-
 }
 
-// ValidateJWT parses and verifies signed tokens
-func ValidateJWT(token string) (*jwt.Token, error) {
-	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-		// signature method checking
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, jwt.ErrSignatureInvalid
-		}
+func ValidateToken(tokenStr, purpose string) (uint, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
 		return jwtSecret, nil
 	})
+
+	if err != nil || !token.Valid {
+		return 0, err
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	if claims["type"] != purpose {
+		return 0, errors.New("invalid token type")
+	}
+
+	userID := uint(claims["user_id"].(float64))
+	return userID, nil
 }
