17c28604248607cea673d584749a04a57664024d
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 08:22:56 +0300

    refactor(model): change username field with email and improved gorm configs

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -3,8 +3,11 @@ package model
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model `json:"-"`
-	Username   string `gorm:"uniqueIndex;not null"`
-	Password   string `gorm:"not null" json:"-"`
-	IsAdmin    bool   `gorm:"default:false"`
+	gorm.Model      `json:"-"` // id, created_at, updated_at, deleted_at
+	Name            string     `gorm:"size:50;not null"       json:"name"`
+	Surname         string     `gorm:"size:50;not null"       json:"surname"`
+	Email           string     `gorm:"size:120;uniqueIndex;not null" json:"email"`
+	Password        string     `gorm:"not null"                json:"-"`
+	IsAdmin         bool       `gorm:"default:false"          json:"is_admin"`
+	IsMailConfirmed bool       `gorm:"default:false"          json:"is_verified"`
 }
