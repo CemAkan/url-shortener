da79c5d9f144879b2f4b1dbadd27c22ab84ed5f4
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 08:34:37 +0300

    refactor(response): change fields via new user model

--- internal/domain/response/auth.go
+++ internal/domain/response/auth.go
@@ -1,12 +1,12 @@
 package response
 
 type LoginResponse struct {
-	ID       uint   `json:"id" example:"1"`
-	Username string `json:"username" example:"asko"`
-	Token    string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."`
+	ID    uint   `json:"id" example:"1"`
+	Email string `json:"email" example:"asko@kusko.com"`
+	Token string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."`
 }
 
 type UserResponse struct {
-	ID       uint   `json:"id" example:"1"`
-	Username string `json:"username" example:"asko"`
+	ID    uint   `json:"id" example:"1"`
+	Email string `json:"email" example:"asko@kusko.com"`
 }
