bde2781a3b953315774941e4e4c2f52261c794c9
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 08:22:17 +0300

    refactor(redis): improved gorm fields

--- internal/domain/model/url.go
+++ internal/domain/model/url.go
@@ -3,9 +3,9 @@ package model
 import "gorm.io/gorm"
 
 type URL struct {
-	gorm.Model  `json:"-"`
-	Code        string `gorm:"uniqueIndex;not null"`
-	OriginalURL string `gorm:"not null"`
-	UserID      uint   `gorm:"index"`
-	TotalClicks int    `gorm:"default:0"`
+	gorm.Model  `json:"-"` // ID, CreatedAt, UpdatedAt, DeletedAt
+	Code        string     `gorm:"uniqueIndex;not null" json:"code"`
+	OriginalURL string     `gorm:"not null"           json:"original_url"`
+	UserID      uint       `gorm:"index"              json:"user_id"`
+	TotalClicks int        `gorm:"default:0"          json:"total_clicks"`
 }
