81ada9c57c9bc3867c4723e4f35ce378dec90873
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 21:13:36 +0300

    feat(mail): change SMTP Mailer service to gomail abstraction instead of wnnessen/go-mail

---
+++ pkg/infrastructure/mail.go
@@ -0,0 +1,42 @@
+package infrastructure
+
+import (
+	"crypto/tls"
+	"github.com/CemAkan/url-shortener/config"
+	"github.com/go-gomail/gomail"
+	"strconv"
+)
+
+type Mailer struct {
+	dialer *gomail.Dialer
+	from   string
+}
+
+var Mail *Mailer
+
+func InitMail() {
+
+	port, _ := strconv.Atoi(config.GetEnv("SMTP_PORT", ""))
+	host := config.GetEnv("SMTP_HOST", "")
+	user := config.GetEnv("SMTP_USER", "")
+	pass := config.GetEnv("SMTP_PASS", "")
+	from := config.GetEnv("SMTP_FROM", "")
+
+	dialer := gomail.NewDialer(host, port, user, pass)
+
+	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	Mail.dialer, Mail.from = dialer, from
+}
+
+// Send sends email
+func (m *Mailer) Send(to, subject, body string) error {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", m.from)
+	msg.SetHeader("To", m.from)
+	msg.SetHeader("Subject", m.from)
+	msg.SetBody("text/html", body)
+
+	return m.dialer.DialAndSend(msg)
+
+}
