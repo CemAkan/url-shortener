65a0eaa34d03c1c3c8ccba3fa15c7b14b63fd239
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 23:06:42 +0300

    feat(middleware): add JWTVerification middleware to validate token and get user id from token

---
+++ internal/middleware/jwt_verification.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"github.com/CemAkan/url-shortener/config"
+	"github.com/CemAkan/url-shortener/internal/domain/response"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTVerification(purpose string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+
+		tokenStr := c.Params("token")
+
+		// Token check
+		token, err := config.ResolveToken(tokenStr, purpose)
+
+		if err != nil || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).JSON(response.ErrorResponse{Error: "Invalid or expired link"})
+
+		}
+
+		// map data claim
+		claims, ok := token.Claims.(jwt.MapClaims)
+
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(response.ErrorResponse{
+				Error: "Invalid token claims",
+			})
+		}
+
+		if claims["type"] != purpose {
+			return c.Status(fiber.StatusUnauthorized).JSON(response.ErrorResponse{Error: "invalid token type"})
+		}
+
+		// userID claim
+
+		userID, ok := claims["user_id"].(float64)
+
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(response.ErrorResponse{
+				Error: "Invalid token user ID",
+			})
+		}
+
+		c.Locals("user_id", uint(userID))
+
+		return c.Next()
+
+	}
+}
