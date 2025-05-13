93a34cc2c3703a0c047f0070a15c732563869766
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 22:03:59 +0300

    featAndRefactor(service): add VerifyLinkGenerator and SendVerificationMail methods and re-edit MailService interface for new method inputs

--- internal/app/mail_service.go
+++ internal/app/mail_service.go
@@ -1,12 +1,41 @@
 package app
 
+import (
+	"github.com/CemAkan/url-shortener/config"
+	"github.com/CemAkan/url-shortener/internal/utils"
+	"github.com/CemAkan/url-shortener/pkg/infrastructure"
+	"time"
+)
+
 type MailService interface {
-	SendVerificationMail(email string) error
-	SendPasswordResetMail(email string) error
+	SendVerificationMail(name, email, verifyLink string) error
+	SendPasswordResetMail(name, email, verifyLink string) error
+	VerifyLinkGenerator(userID uint, baseURL string) (string, error)
 }
 
 type mailService struct{}
 
+// NewMailService constructs mailService struct
 func NewMailService() MailService {
 	return &mailService{}
+}
+
+// SendVerificationMail sends email to verify mail address
+func (s *mailService) SendVerificationMail(name, email, verifyLink string) error {
+	subject := "Welcome! Please confirm your email address"
+	if err := infrastructure.Mail.Send(email, subject, utils.GenerateEmailVerification(name, verifyLink)); er != nil {
+		return err
+	}
+
+}
+
+// VerifyLinkGenerator generates new verification end of the link
+func (s *mailService) VerifyLinkGenerator(userID uint, baseURL string) (string, error) {
+	token, err := config.GenerateToken(userID, 24*time.Hour, "email_verification")
+	if err != nil {
+		return "", err
+	}
+
+	return baseURL + "/verify/" + token, nil
+
 }
