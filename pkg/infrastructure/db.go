ffac57f443337c408c490d2fd4adcaa7bb88730c
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-14 02:53:22 +0300

    fix(database): configure GORM logger to output only to file, prevent terminal logs

--- pkg/infrastructure/db.go
+++ pkg/infrastructure/db.go
@@ -5,7 +5,10 @@ import (
 	"github.com/CemAkan/url-shortener/internal/domain/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -15,9 +18,28 @@ func InitDB() {
 		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),
 	)
 
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+	//log file
+	dbLogFile, err := FileOpener("database")
 	if err != nil {
+		dbLogFile = mainLogFile
+	}
+
+	//logger configuration
+	newLogger := logger.New(
+		log.New(dbLogFile, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Warn,
+			Colorful:      false,
+		},
+	)
+
+	//db open
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil || database == nil {
 		Log.Fatalf("db connection error %v", err.Error())
 	}
 
