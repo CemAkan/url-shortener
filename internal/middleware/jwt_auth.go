08f8563bb0b88633a9b8f5cda1275604a5e30cde
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-13 22:53:09 +0300

    refactor(jwt): implement new jwt solver call via ResolveToken

--- internal/middleware/jwt_auth.go
+++ internal/middleware/jwt_auth.go
@@ -23,7 +23,7 @@ func JWTAuth(purpose string) fiber.Handler {
 		tokenStr = strings.TrimSpace(tokenStr)
 
 		// Token check
-		token, err := config.ValidateToken(tokenStr, purpose)
+		token, err := config.ResolveToken(tokenStr, purpose)
 
 		if err != nil || !token.Valid {
 			return c.Status(fiber.StatusUnauthorized).JSON(response.ErrorResponse{Error: "Invalid or expired token"})
