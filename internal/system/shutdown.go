baa8c81953897ca4a968394703538efba6ac0b0c
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-14 03:07:11 +0300

    fix(system): transfer info logs to infof and add sleep for solving port closing problem

--- internal/system/shutdown.go
+++ internal/system/shutdown.go
@@ -3,16 +3,17 @@ package system
 import (
 	"github.com/CemAkan/url-shortener/pkg/infrastructure"
 	"github.com/gofiber/fiber/v2"
+	"time"
 )
 
 func GracefulShutdown(app *fiber.App) {
-	infrastructure.Log.Info("Starting graceful shutdown...")
+	infrastructure.Log.Infof("Starting graceful shutdown...")
 
 	// Fiber shutdown
 	if err := app.Shutdown(); err != nil {
 		infrastructure.Log.WithError(err).Error("Failed to shutdown Fiber gracefully")
 	} else {
-		infrastructure.Log.Info("Fiber shutdown completed")
+		infrastructure.Log.Infof("Fiber shutdown completed")
 	}
 
 	// Redis shutdown
@@ -20,7 +21,7 @@ func GracefulShutdown(app *fiber.App) {
 		if err := infrastructure.Redis.Close(); err != nil {
 			infrastructure.Log.WithError(err).Error("Failed to close Redis connection")
 		} else {
-			infrastructure.Log.Info("Redis connection closed successfully")
+			infrastructure.Log.Infof("Redis connection closed successfully")
 		}
 	}
 
@@ -30,11 +31,15 @@ func GracefulShutdown(app *fiber.App) {
 		if err := sqlDB.Close(); err != nil {
 			infrastructure.Log.WithError(err).Error("Failed to close DB pool")
 		} else {
-			infrastructure.Log.Info("DB pool closed successfully")
+			infrastructure.Log.Infof("DB pool closed successfully")
 		}
 	} else {
 		infrastructure.Log.WithError(err).Error("Failed to retrieve DB pool handle")
 	}
 
-	infrastructure.Log.Info("Application shutdown complete. Exiting.")
+	infrastructure.Log.Infof("Application shutdown complete. Exiting.")
+
+	//wait to all closings
+	infrastructure.Log.Infof("--- Program will close in 10 seconds ---")
+	time.Sleep(time.Second * 10)
 }
