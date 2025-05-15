7478c9b67b870407463e5bb8dbc3361af3aad881
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-16 02:35:03 +0300

    fix: correct embed.FS usage with exported TemplatesFS interface for proper module resolution

--- email/embed.go
+++ email/embed.go
@@ -1,6 +1,12 @@
 package email
 
-import "embed"
+import (
+	"embed"
+	"io/fs"
+)
 
-//go:embed templates/**/*
-var TemplatesFS embed.FS
+//go:embed templates/**/*.html
+var embeddedTemplates embed.FS
+
+// TemplatesFS exposes embedded templates with FS interface.
+var TemplatesFS fs.FS = embeddedTemplates
