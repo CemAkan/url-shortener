fd6e229abab02032a418d3d7621a2be863eee3b4
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-16 02:35:52 +0300

    feat: implement email render pipeline using embedded templates

---
+++ email/templates/renderer.go
@@ -0,0 +1,27 @@
+package email
+
+import (
+	"bytes"
+	"github.com/CemAkan/url-shortener/email"
+	"html/template"
+)
+
+type EmailData struct {
+	Title            string
+	VerificationLink string
+}
+
+func Render(templateName string, data EmailData) (string, error) {
+	tmpl, err := template.New("base.html").ParseFS(email.TemplatesFS,
+		"templates/base.html",
+		"templates/components/logo.html",
+		"templates/components/footer.html",
+		"templates/transactional/"+templateName+".html",
+	)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, data)
+	return buf.String(), err
+}
