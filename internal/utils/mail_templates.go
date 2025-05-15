a1ea39bfe647e7e0bc7e2c9406ec38b3fa437338
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-16 00:21:24 +0300

    feat(mail-template): add EmailData struct

--- internal/utils/mail_templates.go
+++ internal/utils/mail_templates.go
@@ -1,22 +1,12 @@
 package utils
 
-import "fmt"
-
-func GenerateResetPasswordEmail(username, resetLink string) string {
-	return fmt.Sprintf(`
-		<h2>Password Reset Request</h2>
-		<p>Hello %s,</p>
-		<p>Click the link below to reset your password:</p>
-		<a href="%s">%s</a>
-		<p>This link will expire in 15 minutes.</p>
-	`, username, resetLink, resetLink)
-}
-
-func GenerateEmailVerification(username, verifyLink string) string {
-	return fmt.Sprintf(`
-		<h2>Email Verification</h2>
-		<p>Welcome %s!</p>
-		<p>Please verify your email by clicking the link below:</p>
-		<a href="%s">%s</a>
-	`, username, verifyLink, verifyLink)
+type EmailData struct {
+	Title       string
+	Greeting    string
+	Message     string
+	ButtonText  string
+	ButtonLink  string
+	ShowButton  bool
+	CompanyName string
+	Year        int
 }
