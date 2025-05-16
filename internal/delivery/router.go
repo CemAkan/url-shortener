dfc34b2a52ee8c36f48e7f2662c2b9718b3d6a92
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-16 08:47:34 +0300

    fix(router): change ../../ with app for docker container

--- internal/delivery/router.go
+++ internal/delivery/router.go
@@ -19,7 +19,7 @@ func SetupRoutes(app *fiber.App, authHandler *AuthHandler, urlHandler *URLHandle
 	// public routes (no need jwt)
 
 	//mail assets
-	api.Static("/assets", "../../email/assets")
+	api.Static("/assets", "app/email/assets")
 
 	//health
 	api.Get("/health", Health)
