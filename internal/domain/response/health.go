fa7fb8d558d38b5c71aa62569c5a0a5202d19bfe
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-16 20:40:47 +0300

    feat(domain): add Email field to HealthStatusResponse struct to return email service health status

--- internal/domain/response/health.go
+++ internal/domain/response/health.go
@@ -4,4 +4,5 @@ type HealthStatusResponse struct {
 	Status   string `json:"status" example:"healthy"`
 	Database string `json:"database" example:"ok"`
 	Redis    string `json:"redis" example:"ok"`
+	Email    string `json:"email" example:"ok"`
 }
