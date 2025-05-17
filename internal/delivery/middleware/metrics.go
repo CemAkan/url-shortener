603b6198be5a2ba05505a2e7a147397337b63f25
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 01:12:33 +0300

    feat(middleware): Add Prometheus metrics middleware using fiberprometheus

---
+++ internal/delivery/middleware/metrics.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"github.com/ansrivas/fiberprometheus/v2"
+	"github.com/gofiber/fiber/v2"
+)
+
+// PrometheusMiddleware registers /metrics endpoint and middleware for metrics tracking
+func PrometheusMiddleware(app *fiber.App) {
+	prometheus := fiberprometheus.New("url-shortener")
+	prometheus.RegisterAt(app, "/metrics") // exposes at /metrics
+	app.Use(prometheus.Middleware)         // auto-collects metrics
+}
