1f750a1dfd6e2539bd28f1d283de2b4ce213db64
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 00:43:45 +0300

    fix(import): update middleware import for new path

--- internal/delivery/http/router/router.go
+++ internal/delivery/http/router/router.go
@@ -3,7 +3,7 @@ package router
 import (
 	_ "github.com/CemAkan/url-shortener/docs"
 	"github.com/CemAkan/url-shortener/internal/delivery/http/handler"
-	"github.com/CemAkan/url-shortener/internal/middleware"
+	middleware2 "github.com/CemAkan/url-shortener/internal/delivery/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/swaggo/fiber-swagger"
 )
@@ -15,7 +15,7 @@ func SetupRoutes(app *fiber.App, authHandler *handler.AuthHandler, urlHandler *h
 	api.Get("/docs/*", fiberSwagger.WrapHandler)
 
 	// implement log middleware
-	app.Use(middleware.RequestLogger())
+	app.Use(middleware2.RequestLogger())
 
 	// public routes (no need jwt)
 
@@ -29,9 +29,9 @@ func SetupRoutes(app *fiber.App, authHandler *handler.AuthHandler, urlHandler *h
 	app.Get("/:code", urlHandler.Redirect)
 
 	//verification
-	api.Get("/verify/mail/:token", middleware.JWTVerification("email_verification"), verificationHandler.VerifyMailAddress)
-	api.Get("/verify/password/:token", middleware.JWTVerification("password_reset_verification"), verificationHandler.ResetPasswordTokenResolve)
-	api.Post("/verify/password", middleware.JWTAuth("password_reset_verification"), verificationHandler.ResetPassword)
+	api.Get("/verify/mail/:token", middleware2.JWTVerification("email_verification"), verificationHandler.VerifyMailAddress)
+	api.Get("/verify/password/:token", middleware2.JWTVerification("password_reset_verification"), verificationHandler.ResetPasswordTokenResolve)
+	api.Post("/verify/password", middleware2.JWTAuth("password_reset_verification"), verificationHandler.ResetPassword)
 
 	//auth
 	api.Post("/register", authHandler.Register)
@@ -40,18 +40,18 @@ func SetupRoutes(app *fiber.App, authHandler *handler.AuthHandler, urlHandler *h
 	// -- protected routes (jwt required) --
 
 	//user
-	api.Get("/me", middleware.JWTAuth("auth"), authHandler.Me)
-	api.Get("/password/reset", middleware.JWTAuth("auth"), authHandler.ResetPassword)
+	api.Get("/me", middleware2.JWTAuth("auth"), authHandler.Me)
+	api.Get("/password/reset", middleware2.JWTAuth("auth"), authHandler.ResetPassword)
 
 	//url
-	api.Post("/shorten", middleware.JWTAuth("auth"), urlHandler.Shorten)
-	api.Get("/my/urls", middleware.JWTAuth("auth"), urlHandler.ListUserURLs)
-	api.Get("/my/urls/:code", middleware.JWTAuth("auth"), urlHandler.GetSingleURL)
-	api.Delete("/my/urls/:code", middleware.JWTAuth("auth"), urlHandler.DeleteURL)
-	api.Patch("/my/urls/:code", middleware.JWTAuth("auth"), urlHandler.UpdateURL)
+	api.Post("/shorten", middleware2.JWTAuth("auth"), urlHandler.Shorten)
+	api.Get("/my/urls", middleware2.JWTAuth("auth"), urlHandler.ListUserURLs)
+	api.Get("/my/urls/:code", middleware2.JWTAuth("auth"), urlHandler.GetSingleURL)
+	api.Delete("/my/urls/:code", middleware2.JWTAuth("auth"), urlHandler.DeleteURL)
+	api.Patch("/my/urls/:code", middleware2.JWTAuth("auth"), urlHandler.UpdateURL)
 
 	// Admin routes
-	adminGroup := api.Group("/admin", middleware.JWTAuth("auth"), middleware.AdminOnly())
+	adminGroup := api.Group("/admin", middleware2.JWTAuth("auth"), middleware2.AdminOnly())
 
 	adminGroup.Get("/users", adminHandler.ListUsers)
 	adminGroup.Delete("/users/:id", adminHandler.RemoveUser)
