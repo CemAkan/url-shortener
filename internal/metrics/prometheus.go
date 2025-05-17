30c777aa19f15b044cc7ee4074b99a28b015e4fa
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 02:15:10 +0300

    feat(metrics): Add centralized Prometheus metrics for DB, Redis,Mail, clicks, and latency

---
+++ internal/metrics/prometheus.go
@@ -0,0 +1,36 @@
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	// DB Health
+	DBUp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "db_up",
+		Help: "Database connection status (1 = up, 0 = down)",
+	})
+
+	// Redis Health
+	RedisUp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "redis_up",
+		Help: "Redis connection status (1 = up, 0 = down)",
+	})
+
+	//Mail Health
+	MailUp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "mail_up",
+		Help: "Mail service connection status (1 = up, 0 = down)",
+	})
+
+	// Click Events
+	ClickCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "shortener_click_total",
+		Help: "Total clicks received for short URLs",
+	}, []string{"code"})
+
+	// URL shortening latency
+	ShortenLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "shortener_shorten_latency_seconds",
+		Help:    "Latency histogram for URL shortening",
+		Buckets: prometheus.DefBuckets,
+	})
+)
