80db1f0a741f5ba722d856039d974a7429bc7108
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-19 00:26:52 +0300

    refactor(metrics): remove unnecessary alreadyRegistered check from RegisterAll

--- internal/metrics/prometheus.go
+++ internal/metrics/prometheus.go
@@ -1,29 +1,20 @@
 package metrics
 
-import (
-	"github.com/prometheus/client_golang/prometheus"
-)
+import "github.com/prometheus/client_golang/prometheus"
 
 var (
-
-	// DB Health
+	// Health metrics
 	DBUp = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_up",
-		Help: "Database connection status (1 = up, 0 = down)",
+		Name: "db_up", Help: "Database connection status (1 = up, 0 = down)",
 	})
-
-	// Redis Health
 	RedisUp = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "redis_up",
-		Help: "Redis connection status (1 = up, 0 = down)",
+		Name: "redis_up", Help: "Redis connection status (1 = up, 0 = down)",
 	})
-
-	//Mail Health
 	MailUp = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "mail_up",
-		Help: "Mail service connection status (1 = up, 0 = down)",
+		Name: "mail_up", Help: "Mail service connection status (1 = up, 0 = down)",
 	})
 
+	// HTTP metrics
 	HTTPRequestTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
@@ -31,25 +22,18 @@ var (
 		},
 		[]string{"status_code", "method"},
 	)
-
 	HTTPRequestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "http_request_duration_seconds",
-			Help:    "Histogram of HTTP request duration in seconds.",
+			Help:    "Histogram of HTTP request duration in seconds",
 			Buckets: prometheus.DefBuckets,
 		},
 		[]string{"status_code", "method"},
 	)
 )
 
-var alreadyRegistered bool
-
+// RegisterAll, verilen Registry üzerinde tüm metrikleri Tekrar kayda engelleyecek şekilde register eder.
 func RegisterAll(registry *prometheus.Registry) {
-	if alreadyRegistered {
-		return
-	}
-	alreadyRegistered = true
-
 	registry.MustRegister(
 		DBUp,
 		RedisUp,
