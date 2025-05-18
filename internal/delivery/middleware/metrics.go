e781b05d2b10f699d7358dcd9fdc5292e9c567f2
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 22:30:53 +0300

    refactor(middleware): add metric sending to directly middleware

--- internal/delivery/middleware/metrics.go
+++ internal/delivery/middleware/metrics.go
@@ -2,23 +2,32 @@ package middleware
 
 import (
 	"github.com/ansrivas/fiberprometheus/v2"
-	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
+	"strconv"
+	"time"
+
+	"github.com/CemAkan/url-shortener/internal/metrics"
+	"github.com/gofiber/fiber/v2"
 )
 
 var CustomRegistry = prometheus.NewRegistry()
 
-func PrometheusMiddleware(app *fiber.App) {
-	prometheusMiddleware := fiberprometheus.NewWithRegistry(
-		CustomRegistry,
-		"url-shortener", // namespace
-		"http",          // subsystem
-		"service",       // service label name
-		map[string]string{
-			"env": "dev",
-		},
-	)
-
+func MetricsMiddleware(app *fiber.App) fiber.Handler {
+	prometheusMiddleware := fiberprometheus.NewWithRegistry(CustomRegistry, "url_shortener", "http", "service", nil)
 	prometheusMiddleware.RegisterAt(app, "/metrics")
-	app.Use(prometheusMiddleware.Middleware)
+
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+
+		err := c.Next()
+
+		duration := time.Since(start).Seconds()
+		status := strconv.Itoa(c.Response().StatusCode())
+		method := c.Method()
+
+		metrics.HTTPRequestTotal.WithLabelValues(status, method).Inc()
+		metrics.HTTPRequestDuration.WithLabelValues(status, method).Observe(duration)
+
+		return err
+	}
 }
