245e47e666454bb98349c9f02310faacc13fe8e0
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 03:59:13 +0300

    feat(middleware): Add IP whitelist middleware using config and GetClientIP

--- internal/delivery/middleware/ip_whitelist.go
+++ internal/delivery/middleware/ip_whitelist.go
@@ -1,23 +1,26 @@
 package middleware
 
 import (
-	"github.com/CemAkan/url-shortener/pkg/logger"
-	"github.com/gofiber/fiber/v2"
 	"os"
 	"strings"
+
+	"github.com/CemAkan/url-shortener/pkg/logger"
+	"github.com/CemAkan/url-shortener/pkg/utils"
+	"github.com/gofiber/fiber/v2"
 )
 
 func IPWhitelistMiddleware() fiber.Handler {
-	whitelist := os.Getenv("IP_WHITELIST") // comma-separated
+	whitelist := os.Getenv("IP_WHITELIST") // comma-separated IPs
 	allowedIPs := strings.Split(whitelist, ",")
 
 	return func(c *fiber.Ctx) error {
-		ip := c.IP()
+		ip := utils.GetClientIP(c)
 		for _, allowed := range allowedIPs {
 			if strings.TrimSpace(ip) == strings.TrimSpace(allowed) {
 				return c.Next()
 			}
 		}
+
 		logger.Log.Warnf("Unauthorized IP: %s attempted to access protected route", ip)
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "forbidden"})
 	}
