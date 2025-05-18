5d4954a2846b3ff3376f3ac28e0b2d49633c76d6
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-19 00:35:59 +0300

    refactor(database): adjust app & DB infrastructure configuration

--- internal/infrastructure/db/db.go
+++ internal/infrastructure/db/db.go
@@ -16,7 +16,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_NAME"), os.Getenv("POSTGRES_PORT"),
 	)
 
 	//log file
