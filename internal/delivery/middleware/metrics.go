2338727f7a46bf9edd609d93ee520df996b85887
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-18 19:03:48 +0300

    refactor(middleware): change old middleware with custom one to provide to see custom data metrics

--- internal/delivery/middleware/metrics.go
+++ internal/delivery/middleware/metrics.go
@@ -3,11 +3,22 @@ package middleware
 import (
 	"github.com/ansrivas/fiberprometheus/v2"
 	"github.com/gofiber/fiber/v2"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-// PrometheusMiddleware registers /metrics endpoint and middleware for metrics tracking
+var CustomRegistry = prometheus.NewRegistry()
+
 func PrometheusMiddleware(app *fiber.App) {
-	prometheus := fiberprometheus.New("url-shortener")
-	prometheus.RegisterAt(app, "/api/metrics") // exposes at /metrics
-	app.Use(prometheus.Middleware)             // auto-collects metrics
+	prometheusMiddleware := fiberprometheus.NewWithRegistry(
+		CustomRegistry,
+		"url-shortener", // namespace
+		"http",          // subsystem
+		"service",       // service label name
+		map[string]string{
+			"env": "dev",
+		},
+	)
+
+	prometheusMiddleware.RegisterAt(app, "/metrics")
+	app.Use(prometheusMiddleware.Middleware)
 }
